e0f84721d5755ffab6ec1108b9c2370cda274aa4
Noa <noaisraeli3@gmail.com>
2021-08-07 17:28:23 +0300

    fixed Changed-Email

--- src/app/shared/services/auth.service.ts
+++ src/app/shared/services/auth.service.ts
@@ -10,87 +10,87 @@ import { Router } from "@angular/router";
 
 })
 
-export class AuthService{
+export class AuthService {
 
   private _userData: Observable<firebase.User>;
-  public currentUser: UserData|null;
+  public currentUser: UserData | null;
   private currentUser$ = new BehaviorSubject<UserData>(null);
 
   constructor(
     private afAuth: AngularFireAuth,
     private router: Router,
     private afs: AngularFirestore
-  ){
+  ) {
     this._userData = afAuth.authState;
-    this._userData.subscribe(user=>{
-      if(user){
+    this._userData.subscribe(user => {
+      if (user) {
         this.afs.collection<UserData>('users')
-        .doc<UserData>(user.uid)
-        .valueChanges()
-        .subscribe(currentUser=>{
-          this.currentUser = currentUser;
-          this.currentUser.uid = user.uid;
-          this.currentUser$.next(currentUser);
-        });
+          .doc<UserData>(user.uid)
+          .valueChanges()
+          .subscribe(currentUser => {
+            this.currentUser = currentUser;
+            this.currentUser.uid = user.uid;
+            this.currentUser$.next(currentUser);
+          });
       }
     });
   }
-  CurrentUser(): Observable<UserData>{
+  CurrentUser(): Observable<UserData> {
     return this.currentUser$.asObservable();
   }
 
-  SignUp(email: string, password: string, displayName: string){
-    console.log(email,password,displayName);
-    
-    this.afAuth.createUserWithEmailAndPassword(email,password)
-    .then(res=>{
-      if(res){
-        this.afs.collection('users').doc(res.user.uid)
-        .set({
-          email,
-          displayName
-        }).then(value=>{
-          this.afs.collection<UserData>('users')
-          .doc<UserData>(res.user.uid)
-          .valueChanges()
-          .subscribe(user=>{
-            console.log(user);
-            if(user){
-              this.currentUser$.next(user);
-            }
-          });
-        });
-      }
-    })
-    .catch(err=> console.log(`Something went wrong ${err.message}`));
+  SignUp(email: string, password: string, displayName: string) {
+    console.log(email, password, displayName);
+
+    this.afAuth.createUserWithEmailAndPassword(email, password)
+      .then(res => {
+        if (res) {
+          this.afs.collection('users').doc(res.user.uid)
+            .set({
+              email,
+              displayName
+            }).then(value => {
+              this.afs.collection<UserData>('users')
+                .doc<UserData>(res.user.uid)
+                .valueChanges()
+                .subscribe(user => {
+                  console.log(user);
+                  if (user) {
+                    this.currentUser$.next(user);
+                  }
+                });
+            });
+        }
+      })
+      .catch(err => console.log(`Something went wrong ${err.message}`));
   }
 
-  get userData(): Observable<firebase.User>{
+  get userData(): Observable<firebase.User> {
     return this._userData;
   }
 
-  SignIn(email: string, password: string):void{
+  SignIn(email: string, password: string): void {
     console.log(email, password);
 
     this.afAuth.signInWithEmailAndPassword(email, password)
-    .then(res=>{
-      console.log(res);
-      this._userData = this.afAuth.authState;
+      .then(res => {
+        console.log(res);
+        this._userData = this.afAuth.authState;
 
-      this.afs.collection<UserData>('users')
-      .doc<UserData>(res.user.uid)
-      .valueChanges()
-      .subscribe((user)=>{
-        console.log(user);
-        this.currentUser=user;
-        this.currentUser$.next(this.currentUser);
-        this.router.navigate(['dashboard']);
-      });
-    }).catch(err=> console.log(err.message));
+        this.afs.collection<UserData>('users')
+          .doc<UserData>(res.user.uid)
+          .valueChanges()
+          .subscribe((user) => {
+            console.log(user);
+            this.currentUser = user;
+            this.currentUser$.next(this.currentUser);
+            this.router.navigate(['dashboard']);
+          });
+      }).catch(err => console.log(err.message));
   }
 
-  SignOut(): void{
-    this.afAuth.signOut().then(res=>{
+  SignOut(): void {
+    this.afAuth.signOut().then(res => {
       console.log(res);
       this.currentUser = null;
       this.currentUser$.next(this.currentUser);
@@ -101,46 +101,46 @@ export class AuthService{
 
   ForgotPassword(passwordResetEmail: string) {
     return this.afAuth.sendPasswordResetEmail(passwordResetEmail)
-    .then(() => {
-      window.alert('Password reset email sent, check your inbox.');
-    }).catch((error) => {
-      window.alert(error)
-    })
+      .then(() => {
+        window.alert('Password reset email sent, check your inbox.');
+      }).catch((error) => {
+        window.alert(error)
+      })
   }
 
   // Send email verfificaiton when new user sign up
   SendVerificationMail() {
-    return this.afAuth.currentUser.then((user)=>{
+    return this.afAuth.currentUser.then((user) => {
       return user?.sendEmailVerification();
     })
-    .then(() => {
-      this.router.navigate(['verify-email-address']);
-    })
+      .then(() => {
+        this.router.navigate(['verify-email-address']);
+      })
   }
 
-  updateEmailAddress(newEmail: string, lastEmail: string, password:any){
-    firebase.auth().signInWithEmailAndPassword(lastEmail,password)
-    .then(function(userCredential){
-      console.log("aaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
-      userCredential.user.updateEmail(newEmail);
-    }).catch(function(error) {
-      alert(error);
-      return;
-    })
-    console.log('yes!')
-    this._userData.subscribe(user=>{
-      if(user){
-        this.afs.collection<UserData>('users')
-        .doc<UserData>(user.uid)
-        .update({email:newEmail}).then(()=>{
-          console.log("changed successfully")
+  updateEmailAddress(newEmail: string, lastEmail: string, password: any) {
+    firebase.auth().signInWithEmailAndPassword(lastEmail, password)
+      .then(function (userCredential) {
+        userCredential.user.updateEmail(newEmail);
+      }).then(() => {
+        this._userData.subscribe(user => {
+          if (user) {
+            this.afs.collection<UserData>('users')
+              .doc<UserData>(user.uid)
+              .update({ email: newEmail }).then(() => {
+                console.log("changed successfully")
+              });
+          }
         });
-      }
-    });
+      }).catch(function (error) {
+        alert(error);
+        return;
+      })
+
   }
 }
 
-export interface UserData{
+export interface UserData {
   uid?: string;
   email: string;
   displayName: string;
